1c2dd890dcc58e6074ff87841b746038f929bccb
jackey <jackey@zz.com>
2023-11-04 21:15:04 +0800

    second commit

--- src/main/java/com/ze/Git.java
+++ src/main/java/com/ze/Git.java
@@ -9,5 +9,7 @@ package com.ze;
 public class Git {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("hello world v2.0");
+
     }
 }
