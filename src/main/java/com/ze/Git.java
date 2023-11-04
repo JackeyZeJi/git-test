cca9dd463113a1d94dad9cbdad0c7851d922a955
jackey <jackey@zz.com>
2023-11-04 21:08:19 +0800

    first commit

---
+++ src/main/java/com/ze/Git.java
@@ -0,0 +1,13 @@
+package com.ze;
+
+/**
+ * @program: git-test
+ * @author: Jackey_ZeJi
+ * @create: 2023-11-04 21:00
+ * @description:
+ **/
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
